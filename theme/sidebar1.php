9d43bb857c332a0cb01cf75e1e499e269ed2e9a5
Lucas Montanheiro <lucasmontanheiro@gmail.com>
2015-10-29 20:11:45 -0700

    sidebar urls

--- theme/sidebar1.php
+++ theme/sidebar1.php
@@ -5,78 +5,78 @@ echo '
 	<ul>
 		<h4>Crimes</h4>
 		
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=1">Contra a pessoa</a></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=2">Contra o patrimonio</a></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=3">Contra os costumes</a></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=5">Contravencionais</a></li>
-	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=4">Entorpecentes</a></strong></li>
-	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=51">Estupro</a></strong></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=52">Extorsão mediante seqüestro </a></li>
-	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=58">Furto - outros </a></li></strong>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=59">Furto de veículos </a></li>
-	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=44">Homicídio Culposo</a></strong></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=43">Homicídio culposo por Acidente de Trânsito </a></li>
-	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=41">Homicídio Doloso</a></strong> (<a href="'.$url.'/ocorrencia.php?cr=42">Vítimas de Homicídio Doloso</a>)</li>
-	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=49">Latrocínio</a></strong> (<a href="'.$url.'/ocorrencia.php?cr=50">Vítimas de Latrocínio</a>)</li>
-	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=48">Lesão corporal culposa (outras) </a></strong></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=47">Lesão corporal culposa por Acidente de Trânsito </a></li>
-	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=46">Lesão corporal dolosa </a></strong></li>
-	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=54">Roubo - outros</a></strong></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=56">Roubo a Banco </a></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=57">Roubo de Carga </a></li>
-	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=55">Roubo de veículos </a></strong></li>
-	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=45">Tentativa de homicídio </a></strong></li>
-	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=53">Tráfico de entorpecentes </a></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=1&region=data_capital">Contra a pessoa</a></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=2&region=data_capital">Contra o patrimonio</a></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=3&region=data_capital">Contra os costumes</a></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=5&region=data_capital">Contravencionais</a></li>
+	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=4&region=data_capital">Entorpecentes</a></strong></li>
+	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=51&region=data_capital">Estupro</a></strong></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=52&region=data_capital">Extorsão mediante seqüestro </a></li>
+	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=58&region=data_capital">Furto - outros </a></li></strong>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=59&region=data_capital">Furto de veículos </a></li>
+	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=44&region=data_capital">Homicídio Culposo</a></strong></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=43&region=data_capital">Homicídio culposo por Acidente de Trânsito </a></li>
+	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=41&region=data_capital">Homicídio Doloso</a></strong> (<a href="'.$url.'/ocorrencia.php?cr=42&region=data_capital">Vítimas de Homicídio Doloso</a>)</li>
+	       <li class="sidebar_item"><strong><a href="'.$url.'/ocorrencia.php?cr=49&region=data_capital">Latrocínio</a></strong> (<a href="'.$url.'/ocorrencia.php?cr=50&region=data_capital">Vítimas de Latrocínio</a>)</li>
+	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=48&region=data_capital">Lesão corporal culposa (outras) </a></strong></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=47&region=data_capital">Lesão corporal culposa por Acidente de Trânsito </a></li>
+	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=46&region=data_capital">Lesão corporal dolosa </a></strong></li>
+	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=54&region=data_capital">Roubo - outros</a></strong></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=56&region=data_capital">Roubo a Banco </a></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=57&region=data_capital">Roubo de Carga </a></li>
+	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=55&region=data_capital">Roubo de veículos </a></strong></li>
+	       <strong><li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=45&region=data_capital">Tentativa de homicídio </a></strong></li>
+	       <li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=53&region=data_capital">Tráfico de entorpecentes </a></li>
 
 		<h4>Atividades de polícia judiciária</h4>
 		
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=10">Total de boletins de ocorrência </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=13">Total de inquéritos instaurados </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=11">Total de termos circunstanciados lavrados pela Polícia Civil</a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=12">Total de termos circunstanciados lavrados pela Polícia Militar</a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=10&region=data_capital">Total de boletins de ocorrência </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=13&region=data_capital">Total de inquéritos instaurados </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=11&region=data_capital">Total de termos circunstanciados lavrados pela Polícia Civil</a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=12&region=data_capital">Total de termos circunstanciados lavrados pela Polícia Militar</a></li>
 
 		<h4>Atividades policiais</h4>
 		
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=27">Armas de fogo apreendidas </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=">Laudos Clínicos Laboratoriais Expedidos - IML </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=">Laudos Necroscópicos Expedidos - IML </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=">Laudos Periciais Expedidos - IC</a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=23">Nº de autos de apreensão (art 173 ECA) </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=24">Nº de infratores apreendidos em flagrante </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=25">Nº de infratores apreendidos por mandado </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=21">Nº de pessoas presas em flagrante </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=26">Nº de revistas pessoais/ identificação </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=28">Nº de Veículos Recuperados </a></li>
-			<!--<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=20">Prisões efetuadas (em flagrante + por mandado) </a></li>-->
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=27&region=data_capital">Armas de fogo apreendidas </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=&region=data_capital">Laudos Clínicos Laboratoriais Expedidos - IML </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=&region=data_capital">Laudos Necroscópicos Expedidos - IML </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=&region=data_capital">Laudos Periciais Expedidos - IC</a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=23&region=data_capital">Nº de autos de apreensão (art 173 ECA) </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=24&region=data_capital">Nº de infratores apreendidos em flagrante </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=25&region=data_capital">Nº de infratores apreendidos por mandado </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=21&region=data_capital">Nº de pessoas presas em flagrante </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=26&region=data_capital">Nº de revistas pessoais/ identificação </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=28&region=data_capital">Nº de Veículos Recuperados </a></li>
+			<!--<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=20&region=data_capital">Prisões efetuadas (em flagrante + por mandado) </a></li>-->
 		
 		<h4>Casos envolvendo policiais militares</h4>
 		
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=37">Pessoas feridas em confronto com a polícia militar em serviço </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=38">Pessoas feridas por policiais militares de folga </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=35">Pessoas mortas em confronto com a polícia militar em serviço</a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=36">Pessoas mortas por policiais militares de folga</a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=40">Policiais militares feridos em serviço </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=39">Policiais militares mortos em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=37&region=data_capital">Pessoas feridas em confronto com a polícia militar em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=38&region=data_capital">Pessoas feridas por policiais militares de folga </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=35&region=data_capital">Pessoas mortas em confronto com a polícia militar em serviço</a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=36&region=data_capital">Pessoas mortas por policiais militares de folga</a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=40&region=data_capital">Policiais militares feridos em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=39&region=data_capital">Policiais militares mortos em serviço </a></li>
 		
 		<h4>Casos envolvendo policiais civis</h4>
 
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=16">Pessoas feridas em confronto com a polícia civil em serviço </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=17">Pessoas feridas por policiais civis de folga </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=14">Pessoas mortas em confronto com a polícia civil em serviço </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=15">Pessoas mortas por policiais civis de folga</a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=19">Policiais civis feridos em serviço </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=18">Policiais civis mortos em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=16&region=data_capital">Pessoas feridas em confronto com a polícia civil em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=17&region=data_capital">Pessoas feridas por policiais civis de folga </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=14&region=data_capital">Pessoas mortas em confronto com a polícia civil em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=15&region=data_capital">Pessoas mortas por policiais civis de folga</a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=19&region=data_capital">Policiais civis feridos em serviço </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=18&region=data_capital">Policiais civis mortos em serviço </a></li>
 
 		<h4>Outros</h4>
 		
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=">Exames Clínicos Laboratoriais Realizados - IML </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=">Exames Necroscópicos Realizados - IML </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=">Exames Periciais Realizados - IC </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=9">Não Criminais </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=22">Nº de pessoas presas por mandado </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=6">Outros criminais (não inclui contravenções) </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=7">Total de Crimes Violentos ( Hom.Doloso, Roubo, Latrocínio, Estupro e EMS) </a></li>
-			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=8">Total de delitos </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=&region=data_capital">Exames Clínicos Laboratoriais Realizados - IML </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=&region=data_capital">Exames Necroscópicos Realizados - IML </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=&region=data_capital">Exames Periciais Realizados - IC </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=9&region=data_capital">Não Criminais </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=22&region=data_capital">Nº de pessoas presas por mandado </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=6&region=data_capital">Outros criminais (não inclui contravenções) </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=7&region=data_capital">Total de Crimes Violentos ( Hom.Doloso, Roubo, Latrocínio, Estupro e EMS) </a></li>
+			<li class="sidebar_item"><a href="'.$url.'/ocorrencia.php?cr=8&region=data_capital">Total de delitos </a></li>
 	</ul>
 </div>';
 
